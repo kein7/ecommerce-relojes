6f19b4c70152b831e49c273cfa934f44800be039
kein7 <82776754+kein7@users.noreply.github.com>
2023-12-01 14:09:40 -0300

    Delete server directory

--- server/sv-with-local-mysql.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from './app.js'
-
-import { ProductsModel } from './Models/mysql/products-model.js'
-import { UsersModel } from './Models/mysql/users-model.js'
-
-createApp({ productModel: ProductsModel, userModel: UsersModel})
--- server/utils/readJSON.js
+++
@@ -1,4 +0,0 @@
-// Leer un JSON en ESModules recomendado
-import { createRequire } from 'node:module'
-const require = createRequire(import.meta.url) // crea un require con la dirección del archivo actual
-export const readJSON = (path) => require(path)
